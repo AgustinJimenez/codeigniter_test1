2aff09fd85082d9a162fbdfbc5165340fcc38c88
Agustin <agus.jimenez.caba@gmail.com>
2017-10-24 16:59:58 -0300

    visual changes only

--- application/controllers/backend/Dashboard.php
+++ application/controllers/backend/Dashboard.php
@@ -5,7 +5,7 @@ class Dashboard extends AdminController
 
     public function index()
     {
-        //dd( $this->lang );
+       // dd( $this->lang );
         $this->to_admin_template( 'pages/admin/dashboard', compact('lang') );
             
     }
--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -4,7 +4,7 @@
 	<div class="box-header">
 		<h3 class="box-title"><?= lang('index_heading');?></h3>
 		<div class="pull-right">
-			<a class="btn btn-primary" href="<?= site_url('admin/change_password') ?>">new password</a>
+			<a class="btn btn-primary" href="<?= site_url('admin/change_password') ?>"><?= $this->lang->line('change_password_heading') ?></a>
 		</div>
 	</div>
 
@@ -28,11 +28,18 @@
 						<td><?= htmlspecialchars($user->email,ENT_QUOTES,'UTF-8');?></td>
 						<td>
 							<?php foreach ($user->groups as $group): ?>
-								<?= anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8'), 'class="btn btn-xs btn-link"') ;?><br />
+								<?= anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8'), 'class="btn btn-xs btn-link btn-flat"') ;?><br />
 							<?php endforeach; ?>
 						</td>
-						<td><?= ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link'), 'class="btn btn-success btn-block"') : anchor("auth/activate/". $user->id, lang('index_inactive_link'), 'class="btn btn-warning btn-block"');?></td>
-						<td><?= anchor("admin/users/{$user->id}/edit", '<i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;Edit', 'class="btn btn-primary btn-block"') ;?></td>
+						<td><?= ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link'), 'class="btn btn-success btn-flat"') : anchor("auth/activate/". $user->id, lang('index_inactive_link'), 'class="btn btn-warning btn-flat"');?></td>
+						
+						<td>
+							<div class="btn-group">
+								<a href="<?= "users/{$user->id}/edit" ?>" class="btn btn-flat btn-primary">
+									<i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;Edit
+								</a>
+							</div>
+						</td>
 					</tr>
 				<?php endforeach;?>
 			</tbody>
